3d38f366b2e1bb012ed7bf6150277743ef643a09
Andrew Bondarenko <ent101@outlook.com>
2017-10-27 16:12:23 +0300

    create drag and drop function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { Area01Component } from './area01/area01.component';
 import { Area02Component } from './area02/area02.component';
 import {PanelModule} from 'primeng/primeng';
+import {DragDropModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import {PanelModule} from 'primeng/primeng';
     BrowserModule,
     PanelModule,
     AngularFontAwesomeModule,
+    DragDropModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/area02/area02.component.ts
+++ src/app/area02/area02.component.ts
@@ -32,5 +32,54 @@ export class Area02Component implements OnInit {
   ]
   ngOnInit() {
   }
+  imgDict : {
+    imgPosX:0,
+    imgPosY:0
+  };
+  startMousePosX : number;
+  startMousePosY : number;
+  startImgPosX : number;
+  startImgPosY : number;
+  currentImgPosX : number;
+  currentImgPosY : number;
+  offsetX : number;
+  offsetY : number;
+  zIndex : number = 0;
 
+  dragStart(event) {
+    this.startMousePosX = event.clientX;
+    this.startMousePosY = event.clientY;
+    this.startImgPosX = event.target.x;
+    this.startImgPosY = event.target.y;
+  }
+
+  drag(event) {
+
+    let offsetX = this.startMousePosX - event.clientX;
+    let offsetY = this.startMousePosY - event.clientY;
+    this.currentImgPosX = this.startImgPosX - offsetX;
+    this.currentImgPosY = this.startImgPosY - offsetY;
+
+    event.target.style.position = 'absolute';
+    event.target.style.top = this.currentImgPosY + 'px';
+    event.target.style.left = this.currentImgPosX + 'px';
+    event.target.style.height = 'auto';
+    event.target.style.borderRadius = '10px';    
+/*     console.log(this.startMousePosX + " " + this.startMousePosY + " " + this.startImgPosX + " " + this.startImgPosY);
+    console.log(offsetX + " " + offsetY + ' !!!!'); */
+  }
+
+  dragEnd(event) {
+    this.zIndex = this.zIndex + 1;
+    let offsetX = this.startMousePosX - event.clientX;
+    let offsetY = this.startMousePosY - event.clientY;
+    this.currentImgPosX = this.startImgPosX - offsetX;
+    this.currentImgPosY = this.startImgPosY - offsetY;
+    event.target.style.position = 'absolute';
+    event.target.style.top = this.currentImgPosY + 'px';
+    event.target.style.left = this.currentImgPosX + 'px';
+    event.target.style.height = 'auto';
+    event.target.style.borderRadius = '10px';
+    event.target.style.zIndex = this.zIndex;
+  }
 }
