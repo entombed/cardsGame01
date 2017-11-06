5e0d680a330c968af65cdc1403aa5180ec32d563
Andrew Bondarenko <ent101@outlook.com>
2017-11-06 13:19:35 +0200

    добавлена возможность работы со словами

--- src/app/area02/area02.component.ts
+++ src/app/area02/area02.component.ts
@@ -29,7 +29,11 @@ export class Area02Component implements OnInit {
     '18.png',
     '19.png',
     '20.png'
-  ]
+  ];
+  arrayWords = [
+    'Один', 'Два', 'Три','Четыре', 'Пять', 'Шесть', 'Семь', 'Двадцать пять', 'склеенное', 'небесами', 'перетянуть', 'дачных', 'переспросил', 'пастушеской', 'недопитой', 'любопытной', 'подступить', 'бриллиант', 'претерпели', 'наловчился', 'предводимой', 'дне', 'одушевлены', 'выходка', 'сбрасывая', 'судят', 'властителем', 'отсчитывал', 'контрабандной', 'горничной', 'театральный', 'гранатном', 'угроз', 'засушенные', 'отдыхаешь', 'шестикрылый', 'остыл', 'вскрывается', 'боря', 'печать', 'хлева', 'запрет', 'захождение', 'хозяев', 'колокольни', 'теткой', 'бабушку', 'созналась', 'неравного', 'прищурясь', 'желающий', 'служащих', 'бревенчатыми', 'тонкие', 'есаулы', 'вольностью', 'кузнецов', 'лиходеева', 'заткнута', 'замедленных', 'божьей', 'забавлять', 'дивизиями'
+  ];
+  
   ngOnInit() {
   }
   imgDict : {
@@ -38,44 +42,59 @@ export class Area02Component implements OnInit {
   };
   startMousePosX : number;
   startMousePosY : number;
-  startImgPosX : number;
-  startImgPosY : number;
-  currentImgPosX : number;
-  currentImgPosY : number;
+  startObjPosX : number;
+  startObjPosY : number;
+  currentObjPosX : number;
+  currentObjPosY : number;
   zIndex : number = 0;
+  currentTarget: string = '';
+  
 
   dragStart(event) {
+    this.currentTarget = event.target.className;
     this.startMousePosX = event.clientX;
     this.startMousePosY = event.clientY;
-    this.startImgPosX = event.target.x;
-    this.startImgPosY = event.target.y;
+    this.startObjPosX = event.target.offsetLeft;
+    this.startObjPosY = event.target.offsetTop;
     this.zIndex = this.zIndex + 1;
     event.target.style.zIndex = this.zIndex;
-
   }
 
   drag(event) {
-
-    let offsetX = this.startMousePosX - event.clientX;
-    let offsetY = this.startMousePosY - event.clientY;
-    this.currentImgPosX = this.startImgPosX - offsetX;
-    this.currentImgPosY = this.startImgPosY - offsetY;
-    console.log(this.currentImgPosX + ' !! ' + this.currentImgPosY)
-    event.target.style.top = this.currentImgPosY + 'px';
-    event.target.style.left = this.currentImgPosX + 'px';
-    event.target.style.height = 'auto';
-    event.target.style.borderRadius = '10px';
-    event.target.style.position = 'absolute';
-
- }
+      let offsetX = this.startMousePosX - event.clientX;
+      let offsetY = this.startMousePosY - event.clientY;
+      this.currentObjPosX = this.startObjPosX - offsetX;
+      this.currentObjPosY = this.startObjPosY - offsetY;
+      event.target.style.top = this.currentObjPosY + 'px';
+      event.target.style.left = this.currentObjPosX + 'px';
+      event.target.style.height = 'auto';
+      event.target.style.borderRadius = '10px';
+      event.target.style.position = 'absolute';
+    }
 
   dragEnd(event) {
-    let offsetX = this.startMousePosX - event.clientX;
-    let offsetY = this.startMousePosY - event.clientY;
-    this.currentImgPosX = this.startImgPosX - offsetX;
-    this.currentImgPosY = this.startImgPosY - offsetY;
-    console.log(this.currentImgPosX + ' && ' + this.currentImgPosY)
-    event.target.style.top = this.currentImgPosY + 'px';
-    event.target.style.left = this.currentImgPosX + 'px';
+      let offsetX = this.startMousePosX - event.clientX;
+      let offsetY = this.startMousePosY - event.clientY;
+      let currentObjPosX = this.startObjPosX - offsetX;
+      let currentObjPosY = this.startObjPosY - offsetY;
+      event.target.style.top = currentObjPosY + 'px';
+      event.target.style.left = currentObjPosX + 'px';
   }
+  onClick(event) {
+    if (event.ctrlKey == true) {
+      event.target.style.position = '';
+      event.target.style.top = '';
+      event.target.style.left = '';
+      event.target.style.height = '';
+    }
+  }
+
+  // getCurrentPosition(event) {
+  //   let offsetX = this.startMousePosX - event.clientX;
+  //   let offsetY = this.startMousePosY - event.clientY;
+  //   let currentObjPosX = this.startObjPosX - offsetX;
+  //   let currentObjPosY = this.startObjPosY - offsetY;
+  //   let dicPosotion = {"posX":currentObjPosX, "posY":currentObjPosY};
+  //   return dicPosotion;
+  // }
 }
